4b12254baffa660a937eca0f54cff13fc325daf2
Vladimir Zaikin <friend.zva@yandex.ru>
2026-09-16 14:31:38 +0300

    fix(fpga): serdes config
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

--- fpga/project/src/serdes/serdes_tmp.v
+++ fpga/project/src/serdes/serdes_tmp.v
@@ -1,12 +1,12 @@
 //Copyright (C)2014-2026 Gowin Semiconductor Corporation.
 //All rights reserved.
 //File Title: Template file for instantiation
-//Tool Version: V1.9.12.03
+//Tool Version: V1.9.12.02
 //IP Version: 1.0
 //Part Number: GW5AST-LV138FPG676AC1/I0
 //Device: GW5AST-138
 //Device Version: B
-//Created Time: Mon Aug 10 15:50:45 2026
+//Created Time: Wed Sep 16 23:28:04 2026
 
 //Change the instance name and port connections to the signal names
 //--------Copy here to design--------
